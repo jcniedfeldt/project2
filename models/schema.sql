7fe527a06a2e72336923854d8ffc3f3de350aaf2
John N <43926285+jcniedfeldt@users.noreply.github.com>
2019-02-13 21:10:22 -0700

    Update database

--- models/schema.sql
+++ models/schema.sql
@@ -10,6 +10,7 @@ CREATE DATABASE fitnessapp;
 CREATE TABLE users(
     id INT AUTO_INCREMENT NOT NULL,
     account_created TIMESTAMP,
+    username TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     birthday DATE NOT NULL,
@@ -18,20 +19,19 @@ CREATE TABLE users(
     PRIMARY KEY (id)
 );
 
-CREATE TABLE users(
+CREATE TABLE posts(
     id AUTO_INCREMENT NOT NULL,
     post_time TIMESTAMP,
     user_id INT NOT NULL,
     dest_user_id INT NOT NULL,
+    title TEXT NULL,
     post TEXT NOT NULL,
     -- reactions_id int NULL,
-    weight FLOAT NOT NULL,
     PRIMARY KEY (id)
 );
 
 -- CREATE TABLE reactions(
 --     id AUTO_INCREMENT NOT NULL,
---     post_time TIMESTAMP,
 --     user_id INT NOT NULL,
 --     dest_user_id INT NOT NULL,
 --     post TEXT NOT NULL,
