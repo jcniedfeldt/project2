c79569ea2315f258ba9b5aa050d38388fc5b722b
John Niedfeldt <jackniedfeldt@gmail.com>
2019-02-23 13:01:25 -0700

    Eslint fixed and controllers folder deleted. Prep for merge with master.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -125,9 +125,9 @@ module.exports = function(app) {
       });
     // res.render("profile");
   });
-  app.post("/api/login", function(req, res) {
-    // res.render("profile");
-  });
+  // app.post("/api/login", function(req, res) {
+  //   // res.render("profile");
+  // });
   app.post("/api/signup", function(req, res) {
     // res.render("profile");
     db.users
